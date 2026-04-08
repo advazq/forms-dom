c91dcb8e3cbea333c6c6381acc4cd2aafafa3ced
Adrián Vázquez <advzq@pm.me>
2026-04-09 01:59:23 +0200

    started DOM manipulation

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Lógica para ocultar menú de navegación (sidebar)
+const collapseMenuButton = document.querySelector(".collapse-button");
+
+collapseMenuButton.addEventListener("click", function () {
+  console.log("Button clicked!");
+});
