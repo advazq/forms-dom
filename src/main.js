62c68dd30d123cbe1837d2960da30902fb5a6a73
Adrián Vázquez <advzq@pm.me>
2026-04-10 01:05:48 +0200

    added collapse button logic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 // Lógica para ocultar menú de navegación (sidebar)
 const collapseMenuButton = document.querySelector(".collapse-button");
+const searchBar = document.querySelector(".menu-search");
+const textMenuItems = document.querySelectorAll(".menu-link span");
+
+const collapseMenu = (links) => {
+  links.forEach((link) => (link.textContent = ""));
+  searchBar.textContent = "";
+};
 
 collapseMenuButton.addEventListener("click", function () {
-  console.log("Button clicked!");
+  collapseMenu(textMenuItems);
+  console.log("Botón collapse pulsado");
 });
