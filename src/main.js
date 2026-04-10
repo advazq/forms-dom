d0813372c8c3938d1d22d7f90772a04cb3448569
Adrián Vázquez <advzq@pm.me>
2026-04-10 20:21:26 +0200

    added collapse navigation-bar animation

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // Lógica para ocultar menú de navegación (sidebar)
 const collapseMenuButton = document.querySelector(".collapse-button");
+const navigationBar = document.querySelector(".navbar");
 const searchBar = document.querySelector(".menu-search");
 const textMenuItems = document.querySelectorAll(".menu-link span");
 
 const collapseMenu = (links) => {
   links.forEach((link) => link.classList.toggle("hidden"));
   searchBar.classList.toggle("hidden");
+  collapseMenuButton.classList.toggle("rotate-icon");
+  navigationBar.classList.toggle("collapsed-bar");
 };
 
 collapseMenuButton.addEventListener("click", function () {
